0e46a7aa79fd7c8683f03852b4a09373ee6667c8
yanisin13 <quin.vannatter@gmail.com>
2015-09-11 13:24:50 -0400

    Start of Program

---
+++ FileBuilderV2/FileBuilderV2/FileHider.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace FileBuilderV2
+{
+    partial class FileHider
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.butClose = new System.Windows.Forms.Button();
+            this.labInfo = new System.Windows.Forms.Label();
+            this.pBpercent = new System.Windows.Forms.ProgressBar();
+            this.butStart = new System.Windows.Forms.Button();
+            this.butCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FileBuilderV2.Properties.Resources._4T9zBpp8c;
+            this.pictureBox1.Location = new System.Drawing.Point(131, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // butClose
+            // 
+            this.butClose.Location = new System.Drawing.Point(312, 271);
+            this.butClose.Name = "butClose";
+            this.butClose.Size = new System.Drawing.Size(75, 23);
+            this.butClose.TabIndex = 1;
+            this.butClose.Text = "Close";
+            this.butClose.UseVisualStyleBackColor = true;
+            this.butClose.Click += new System.EventHandler(this.butClose_Click);
+            // 
+            // labInfo
+            // 
+            this.labInfo.AutoSize = true;
+            this.labInfo.Location = new System.Drawing.Point(9, 9);
+            this.labInfo.Name = "labInfo";
+            this.labInfo.Size = new System.Drawing.Size(58, 13);
+            this.labInfo.TabIndex = 2;
+            this.labInfo.Text = "information";
+            // 
+            // pBpercent
+            // 
+            this.pBpercent.Location = new System.Drawing.Point(12, 300);
+            this.pBpercent.Name = "pBpercent";
+            this.pBpercent.Size = new System.Drawing.Size(375, 23);
+            this.pBpercent.TabIndex = 3;
+            // 
+            // butStart
+            // 
+            this.butStart.Location = new System.Drawing.Point(150, 271);
+            this.butStart.Name = "butStart";
+            this.butStart.Size = new System.Drawing.Size(75, 23);
+            this.butStart.TabIndex = 4;
+            this.butStart.Text = "Start";
+            this.butStart.UseVisualStyleBackColor = true;
+            this.butStart.Click += new System.EventHandler(this.butStart_Click);
+            // 
+            // butCancel
+            // 
+            this.butCancel.Location = new System.Drawing.Point(231, 271);
+            this.butCancel.Name = "butCancel";
+            this.butCancel.Size = new System.Drawing.Size(75, 23);
+            this.butCancel.TabIndex = 5;
+            this.butCancel.Text = "Cancel";
+            this.butCancel.UseVisualStyleBackColor = true;
+            this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
+            // 
+            // FileHider
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 335);
+            this.ControlBox = false;
+            this.Controls.Add(this.labInfo);
+            this.Controls.Add(this.butCancel);
+            this.Controls.Add(this.butStart);
+            this.Controls.Add(this.pBpercent);
+            this.Controls.Add(this.butClose);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FileHider";
+            this.Text = "File Hider";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button butClose;
+        private System.Windows.Forms.Label labInfo;
+        private System.Windows.Forms.ProgressBar pBpercent;
+        private System.Windows.Forms.Button butStart;
+        private System.Windows.Forms.Button butCancel;
+    }
+}
+
---
+++ FileBuilderV2/FileBuilderV2/FileHider.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace FileBuilderV2
+{
+    public partial class FileHider : Form
+    {
+        int state = 0;
+        string hideFile = "";
+        string keyFile = "";
+        string mapFile = "";
+
+        int totalSize = 0;
+        int completedSize = 0;
+
+        public FileHider()
+        {
+            InitializeComponent();
+            butStart.Enabled = false;
+            butCancel.Enabled = false;
+            this.AllowDrop = true;
+            this.DragDrop += FileHider_DragDrop;
+            this.DragEnter += FileHider_DragEnter;
+            labInfo.Text  = "Click and drag key or\nfile to be hidden on to the plus.";
+        }
+
+        void FileHider_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void FileHider_DragDrop(object sender, DragEventArgs e)
+        {
+            string file = ((string [])e.Data.GetData(DataFormats.FileDrop))[0];
+            if (state == 0)
+            {
+                butCancel.Enabled = true;
+                if (file.Split('.').Length != 2 || file.Split('.')[1] != "fkey")
+                {
+                    hideFile = file;
+                    labInfo.Text  = "File to be hidden has been added.\nPlease add map file.";
+                }
+                else
+                {
+                    keyFile = file;
+                    labInfo.Text  = "Key has been added.\nPlease add map file.";
+                }
+                state = 1;
+            }
+            else if(state==1)
+            {
+                labInfo.Text = "Map file added.\n";
+                if (keyFile != "") labInfo.Text += "Press start to create file.";
+                else labInfo.Text += "Press start to create key.";
+                mapFile = file;
+                state = 2;
+                butStart.Enabled = true;
+            }
+        }
+
+        void HideFile(string hideFileLoc, string mapFileLoc, FileHider caller)
+        {
+                Stream mapStream = File.Open(mapFileLoc, FileMode.Open);
+                byte[] mapBytes = new byte[mapStream.Length];
+                mapStream.Read(mapBytes, 0, mapBytes.Length);
+                mapStream.Close();
+
+                Stream fileStream = File.Open(hideFileLoc, FileMode.Open);
+                byte[] fileBytes = new byte[fileStream.Length];
+                fileStream.Read(fileBytes, 0, fileBytes.Length);
+                fileStream.Close();
+
+                List<List<int>> byteLocations = new List<List<int>>();
+                for (int i = 0; i < 256; i++)
+                {
+                    byteLocations.Add(new List<int>());
+                    for (int h = 0; h < mapBytes.Length; h++)
+                    {
+                        if (mapBytes[h] == i)
+                        {
+                            byteLocations[i].Add(h);
+                        }
+                    }
+                }
+                List<int> missingBytes = new List<int>();
+                for (int i = 0; i < byteLocations.Count; i++)
+                {
+                    if (byteLocations[i].Count == 0)
+                    {
+                        missingBytes.Add(i);
+                    }
+                }
+                bool validMap = true;
+                if (missingBytes.Count != 0)
+                {
+                    for (int i = 0; i < missingBytes.Count && validMap; i++)
+                    {
+                        for (int h = 0; h < fileBytes.Length && validMap; h++)
+                        {
+                            if (missingBytes[i] == fileBytes[h])
+                            {
+                                validMap = false;
+                            }
+                        }
+                    }
+                }
+                if (validMap)
+                {
+                    List<int> locations = new List<int>();
+                    List<int> rawData = new List<int>();
+                    completedSize = 0;
+                    totalSize = fileBytes.Length;
+                    foreach (byte b in fileBytes)
+                    {
+                        completedSize++;
+                        List<int> newBytes = ConvertDec(FindLocation(byteLocations, b));
+                        locations.Add(newBytes.Count);
+                        foreach (int i in newBytes) rawData.Add(i);
+                    }
+                    List<int> locSize = ConvertDec(locations.Count);
+                    foreach (int i in locSize) rawData.Add(i);
+                    rawData.Add(locSize.Count);
+                    string fileName = "";
+                    string extention = "";
+                    if (hideFileLoc.Split('.').Length > 1)
+                    {
+                        fileName = hideFileLoc.Split('.')[0] + ".fkey";
+                        extention = hideFileLoc.Split('.')[1];
+                        byte[] extBytes = ASCIIEncoding.ASCII.GetBytes(extention);
+                        foreach (byte b in extBytes) rawData.Add(b);
+                        rawData.Add(extBytes.Length);
+                    }
+                    else
+                    {
+                        fileName = hideFileLoc + ".fkey";
+                        rawData.Add(0);
+                    }
+                    foreach (int i in rawData) locations.Add(i);
+                    byte[] final = new byte[locations.Count];
+                    for (int i = 0; i < locations.Count; i++) final[i] = (byte)locations[i];
+                    File.WriteAllBytes(fileName, final);
+                    MessageBox.Show("Key Created.");
+                }
+                else
+                {
+                    MessageBox.Show("Map is not sufficant.");
+                }
+            state = 0;
+        }
+
+        List<int> ConvertDec (int number)
+        {
+            List<int> numbers = new List<int>();
+            for(int i = number;i>0;i = (int)Math.Floor((decimal)i/256))
+            {
+                numbers.Add(i % 256);
+            }
+            numbers.Reverse();
+            return numbers;
+        }
+
+        int ConvertByte(List<int> digits)
+        {
+            int result = 0;
+            for (int i = 0; i < digits.Count; i++) result += digits[i] * (int)Math.Pow(256, digits.Count - 1 - i);
+            return result;
+        }
+
+        int FindLocation (List<List<int>> locations, int byteNeeded)
+        {
+            Random ran = new Random();
+            return locations[byteNeeded][ran.Next(locations[byteNeeded].Count)];
+        }
+
+        void BuildFile(string keyFileLoc, string mapFileLoc, FileHider caller)
+        {
+            try
+            {
+                Stream mapStream = File.Open(mapFileLoc, FileMode.Open);
+                byte[] mapBytes = new byte[mapStream.Length];
+                mapStream.Read(mapBytes, 0, mapBytes.Length);
+                mapStream.Close();
+
+                Stream keyStream = File.Open(keyFileLoc, FileMode.Open);
+                byte[] keyBytes = new byte[keyStream.Length];
+                keyStream.Read(keyBytes, 0, keyBytes.Length);
+                keyStream.Close();
+
+                int extSize = keyBytes[keyBytes.Length - 1];
+                List<byte> extNums = new List<byte>();
+                for (int i = keyBytes.Length - extSize - 1; i < keyBytes.Length - 1; i++) extNums.Add(keyBytes[i]);
+                string extention = String.Join("",Encoding.ASCII.GetChars(extNums.ToArray()));
+                int locNumSize = keyBytes[keyBytes.Length - extSize - 2];
+                List<int> locNums = new List<int>();
+                for (int i = keyBytes.Length - locNumSize - extSize - 2; i < keyBytes.Length - extSize - 2; i++) locNums.Add(keyBytes[i]);
+                int startNum = ConvertByte(locNums);
+                int startCount = startNum;
+                List<int> locations = new List<int>();
+                for (int i = 0; i < startNum; i++)
+                {
+                    List<int> newInt = new List<int>();
+                    for (int h = startCount; h < keyBytes[i] + startCount; h++)
+                    {
+                        newInt.Add(keyBytes[h]);
+                    }
+                    startCount += keyBytes[i];
+                    locations.Add(ConvertByte(newInt));
+                }
+                byte[] final = new byte[locations.Count];
+                totalSize = final.Length;
+                completedSize = 0;
+                for (int i = 0; i < final.Length; i++)
+                {
+                    final[i] = mapBytes[locations[i]];
+                    completedSize = i;
+                }
+                string fileName = keyFileLoc.Split('.')[0] + "." + extention;
+                File.WriteAllBytes(fileName, final);
+                MessageBox.Show("File Created.");
+            }
+            catch
+            {
+                MessageBox.Show("File failed to be created.");
+            }
+            state = 0;
+        }
+
+        private void butClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void butStart_Click(object sender, EventArgs e)
+        {
+            if(state==2)
+            {
+                Thread thread;
+                if (hideFile != "") 
+                {
+                    thread = new Thread(delegate() { HideFile(hideFile, mapFile, this); });
+                    thread.Start();
+                }
+                else
+                {
+                    thread = new Thread(delegate() { BuildFile(keyFile, mapFile, this); });
+                    thread.Start();
+                }
+                butCancel.Enabled = false;
+                butStart.Enabled = false;
+                while(thread.IsAlive)
+                {
+                    if(totalSize>0)
+                    {
+                        pBpercent.Value = (int)((float)completedSize / (float)totalSize  * 100);
+                    }
+                    Thread.Sleep(1);
+                }
+                pBpercent.Value = 0;
+                hideFile = "";
+                mapFile = "";
+                keyFile = "";
+                labInfo.Text = "Click and drag key or\nfile to be hidden on to the plus.";
+            }
+        }
+
+        private void butCancel_Click(object sender, EventArgs e)
+        {
+            if(state!=0)
+            {
+                butStart.Enabled = false;
+                butCancel.Enabled = false;
+                state = 0;
+                hideFile = "";
+                mapFile = "";
+                keyFile = "";
+                labInfo.Text = "Click and drag key or\nfile to be hidden on to the plus.";
+            }
+        }
+    }
+}
